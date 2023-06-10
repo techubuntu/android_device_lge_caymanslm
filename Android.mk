f26ab9becf875166f38688e9358c94df8fc860aa
salah5541 <125756690+salah5541@users.noreply.github.com>
2023-06-10 05:30:18 +0200

    Add files via upload

--- Android.mk
+++ Android.mk
@@ -1,10 +1,12 @@
-# Android device tree for lge LM-G910 (caymanslm)
-
-```
 #
 # Copyright (C) 2023 The Android Open Source Project
 # Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),caymanslm)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
-# Android device tree for lge LM-G910 (caymanslm)
-
-```
 #
 # Copyright (C) 2023 The Android Open Source Project
 # Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+add_lunch_combo omni_caymanslm-user
+add_lunch_combo omni_caymanslm-userdebug
+add_lunch_combo omni_caymanslm-eng
